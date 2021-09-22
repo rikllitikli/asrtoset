c0f3a6c447ed60e94bc3803591c41ae976b0c3a5
Kirill Pushkarow <kirrrilll98@gmail.com>
2021-09-22 15:58:37 +0300

    adaptive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $('.owl-carousel').owlCarousel({
+    $('.slider').owlCarousel({
         loop:true,
         margin:0,
         nav:true,
@@ -16,50 +16,35 @@ $(document).ready(function(){
             }
         }
     });
+    $('.slider_two').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            1000:{
+                items:4
+            },
+            992:{
+                items:3
+            },
+            576:{
+                items:3
+            },
+            575:{
+                items:2
+            },
+            414:{
+                items:2
+            },
+            413:{
+                items:1
+            },
+            0:{
+                items:1
+            },
+        }
+    });
 });
 
-$('.btn_q').on('click', function(){
-    $('.text_q').fadeToggle('slow');
-    //or
-    //$(this).next().fadeToggle('slow');
-});
-$('.btn_w').on('click', function(){
-    $('.text_w').fadeToggle('slow');
-    //or
-    //$(this).next().fadeToggle('slow');
-});
-$('.btn_e').on('click', function(){
-    $('.text_e').fadeToggle('slow');
-    //or
-    //$(this).next().fadeToggle('slow');
-});
-$('.btn_r').on('click', function(){
-    $('.text_r').fadeToggle('slow');
-    //or
-    //$(this).next().fadeToggle('slow');
-});
-$('.btn_t').on('click', function(){
-    $('.text_t').fadeToggle('slow');
-    //or
-    //$(this).next().fadeToggle('slow');
-});
-$('.btn_y').on('click', function(){
-    $('.text_y').fadeToggle('slow');
-    //or
-    //$(this).next().fadeToggle('slow');
-});
-$('.btn_u').on('click', function(){
-    $('.text_u').fadeToggle('slow');
-    //or
-    //$(this).next().fadeToggle('slow');
-});
-$('.btn_i').on('click', function(){
-    $('.text_i').fadeToggle('slow');
-    //or
-    //$(this).next().fadeToggle('slow');
-});
-$('.btn_o').on('click', function(){
-    $('.text_o').fadeToggle('slow');
-    //or
-    //$(this).next().fadeToggle('slow');
-});
